f144582ebc1f7097c9ca3b3c7097b5034ac89c5e
Sevenings <lvlassis.2@gmail.com>
2025-06-02 00:35:17 -0300

    fix: gunicorn

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,7 +4,7 @@ cd /opt/master_mind
 
 mode=$1
 if [ $mode == "--local" ]; then
-    gunicorn -b 127.0.0.1:8000 main:app
+    python -m gunicorn -b 127.0.0.1:8000 main:app
 else
-    gunicorn -b 0.0.0.0:8000 main:app
+    python -m gunicorn -b 0.0.0.0:8000 main:app
 fi
