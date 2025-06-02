df5301642210ca787a9e36890cb9362469c69c15
Sevenings <lvlassis.2@gmail.com>
2025-06-02 01:48:20 -0300

    fix

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 cd /opt/master_mind
-.venv/bin/gunicorn -b 0.0.0.0:8000 main:app
+.venv/bin/gunicorn \
+  -k uvicorn.workers.UvicornWorker \
+  -w 4 \
+  -b 0.0.0.0:8000 \
+  main:app
