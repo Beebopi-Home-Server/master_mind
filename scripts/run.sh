48d19f0226dd836cecb191e9bf948a442f9bdf30
Sevenings <lvlassis.2@gmail.com>
2025-06-02 01:24:40 -0300

    fix

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,6 +5,8 @@ cd /opt/master_mind
 mode=$1
 if [ $mode == "--local" ]; then
     python -m gunicorn -b 127.0.0.1:8000 main:app
+else if [ $mode == "--docker" ]; then
+    python -m gunicorn -b 172.17.0.1:8000 main:app
 else
     python -m gunicorn -b 0.0.0.0:8000 main:app
 fi
